d07f83ed0e5428d4b9d08dfc1ddcb5d47d4d8572
Germain <ger.truong@gmail.com>
2022-12-03 18:57:00 +0100

    ✅ Solve day 1

--- day1/main.py
+++ day1/main.py
@@ -1,13 +1,29 @@
 def test_example():
-    assert solve('example.txt') == 24000;
+    assert solve_first_puzzle('example.txt') == 24000;
+    assert solve_second_puzzle('example.txt') == 45000;
 
 def test_input():
-    assert solve('input.txt') == 66616;
+    assert solve_first_puzzle('input.txt') == 66616;
+    assert solve_second_puzzle('input.txt') == 199172;
 
-def solve(file_name):
+def solve_first_puzzle(file_name):
     lines = parse_file(file_name);
     
-    all_sums = [];
+    elves_calories = get_all_elves_calories(lines);
+    max_calories = max(elves_calories);
+
+    return max_calories;
+
+def solve_second_puzzle(file_name):
+    lines = parse_file(file_name);
+    
+    elves_calories = get_all_elves_calories(lines);
+    elves_calories.sort();
+
+    return sum(elves_calories[-3:]);
+
+def get_all_elves_calories(lines):
+    elves_calories = [];
     sum_index = 0;
     for line in lines:
         if line == '\n':
@@ -15,12 +31,11 @@ def solve(file_name):
         else:
             calories = int(line.replace('\n', ''));
             try:
-                all_sums[sum_index] += calories;
+                elves_calories[sum_index] += calories;
             except IndexError:
-                all_sums.insert(sum_index, calories);
+                elves_calories.insert(sum_index, calories);
 
-    max_calories = max(all_sums);
-    return max_calories;
+    return elves_calories;
 
 def parse_file(file_name): 
     file = open(f'day1/input/{file_name}', 'r');
